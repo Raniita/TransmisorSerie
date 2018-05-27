68f81b1bdf636823a9baafd458c1c894f9ed24b3
Enrique 'Rani <30501761+Raniita@users.noreply.github.com>
2018-05-27 19:03:36 +0200

    Funcionalidad comprobada

--- transmisor.vhd
+++ transmisor.vhd
@@ -48,19 +48,24 @@ architecture Behavioral of transmisor is
 signal state : std_logic_vector(1 downto 0):= (others => '0');  -- Estado actual   0 => IDLE
 signal BAUD_SELEC : std_logic_vector(1 downto 0);               -- Selector baud
 signal DATA : std_logic_vector(7 downto 0);                     -- RegData
-signal BTN : std_logic;                                         -- Boton
+--signal BTN : std_logic;                                         -- Boton
 signal SENAL_MUESTREO: std_logic := '0';
 signal BIT_PARIDAD :  std_logic;
+signal TSR : std_logic;
+
+signal MENSAJE : std_logic_vector(9 downto 0);                 -- Mensaje a enviar
 
 signal count : std_logic_vector(11 downto 0);                  -- Cuenta de clk
 signal aux_count : std_logic_vector(11 downto 0);              -- Cuenta aux de clk
 
 signal bauds : std_logic_vector(11 downto 0);                  -- Numero de clk para el baudrate
 
+signal bits_send : std_logic_vector(3 downto 0);               -- length MENSAJE
+
 begin
 
 -- Contador de ciclos de reloj
-process(clk,a_reset)
+clk_counter:process(clk,a_reset)
 begin
     if(a_reset = '1') then
         count <= (others => '0');
@@ -72,6 +77,7 @@ begin
         if (state = "01") then
             BAUD_SELEC <= BAUD_SELEC_IN;
             DATA <= DATA_IN;
+            MENSAJE <= BIT_PARIDAD & DATA(7 downto 0) & '0';
             -- BTN <= BTN_IN;                               No hace falta registrarlo
             if(count = bauds) then
                 count <= (others => '0');
@@ -82,20 +88,27 @@ begin
                 SENAL_MUESTREO <= '0';
                 count <= aux_count;
             end if;
-         elsif(state = "00") then 
-            -- TX_ready <= '1';
+         elsif(state = "10") then 
+             if(count = bauds) then
+                count <= (others => '0');
+                aux_count <= (others => '0');
+                SENAL_MUESTREO <= '1';
+            else
+                aux_count <= aux_count + '1';
+                SENAL_MUESTREO <= '0';
+                count <= aux_count;
+            end if;
         end if;
     end if;
 end process;
 
-BIT_PARIDAD <= DATA(7) xor DATA(6) xor DATA(5) xor DATA(4) xor DATA(3) xor DATA(2) xor 
-DATA(1) xor DATA(0);
+BIT_PARIDAD <= DATA(7) xor DATA(6) xor DATA(5) xor DATA(4) xor DATA(3) xor DATA(2) xor DATA(1) xor DATA(0);
 
 -- Selector de Bauds
-process(BAUD_SELEC,a_reset)
+selec_bauds:process(BAUD_SELEC,a_reset)
 begin
     if(a_reset = '1') then
-        -- BAUD_SELEC <= "00";
+        BAUD_SELEC <= "00";
         bauds <= (others => '0');
     elsif(state = "01") then      -- Solo hará si se esta en estado 1
         case BAUD_SELEC is
@@ -108,17 +121,52 @@ begin
 end process;
 
 -- Pulsador de boton para estado
-process(BTN_IN,a_reset)
+button:process(BTN_IN,state)
+begin
+    if(state = "00" and BTN_IN'event and BTN_IN = '1') then
+            state <= "01";
+    end if;
+end process;
+
+-- Shifter
+shifter:process(senal_muestreo,a_reset)
 begin
     if(a_reset = '1') then
-        state <= (others => '0');
+        bits_send <= (others => '0');
+    elsif(senal_muestreo'event and senal_muestreo = '1') then
+        case state is
+        when "01" =>
+            bits_send <= "0000";
+            state <= "10";
+            TSR <= MENSAJE(0);
+            bits_send <= bits_send + '1';
+        when "10" => 
+            if (bits_send = "1010") then
+                bits_send <= (others => '0');
+                TSR <= '1';
+                state <= "00";
+            else
+                MENSAJE <= MENSAJE(0) & MENSAJE(9 downto 1);
+                TSR <= MENSAJE(0);
+                bits_send <= bits_send + '1';
+            end if;
+        when others =>
+                state <= "00"; 
+           
+        end case;
+    end if;
+end process;
+
+TX_out <= TSR;
+
+-- TX_ready
+txready:process(state)
+begin
+    if(state = "00") then
         TX_ready <= '1';
-    elsif(state = "00" and BTN_IN'event and BTN_IN = '1') then
-        state <= "01";
+    else 
         TX_ready <= '0';
     end if;
 end process;
 
 end Behavioral;
-
-
